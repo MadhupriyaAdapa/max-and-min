47d4a1839940e427c67696e4457b88edb6b4a53a
ADAPA MADHU PRIYA <madhupriyaadapa05@gmail.com>
2025-07-01 10:40:09 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,12 @@
+a=[1,2,3,1,2,3,4,45,1,2,3]
+max=a[0]
+n=len(a)
+for i in range(n):
+    if a[i]>max:
+        max=a[i]
+print(max)
+min=a[0]
+for i in range(n):
+    if a[i]<min:
+        min=a[i]
+print(min)
